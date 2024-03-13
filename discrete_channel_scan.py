6d3562083e8f960ae4eb11095af28dd6c612090c
Max Fairchild <Max.Fairchild@lasp.colorado.edu>
2024-03-13 16:07:38 -0600

    created a script that steps through the channels discretely. This script does not focus on channels that have current broadcasts of remote ID. instead it continues to scan through all the channels.

---
+++ discrete_channel_scan.py
@@ -0,0 +1,95 @@
+__version__ = "0.1.0"
+
+# import numpy as np
+import subprocess
+import time
+import pyshark
+
+
+def get_channel_list():
+    ''' This function returns a list of the WiFi channels that will be scanned in the order
+    are scanned in a 5 second interval.'''
+
+    #channels = {'TwoGHz':['1', '6', '11'], \
+                #'FiveGHz1': ['36','40,'44','48'], \
+                #'FiveGHz2':['149', '153', '157', '161'], \
+                #'Scan_time':[0.5, 0.25]}
+    channels = ['1', '6', '11','36','40','44','48','1', '6', '11','149', '153', '157', '161']
+                               
+    return channels
+
+def main():
+
+    #determining the interface name
+    # Use MAC address on the back of the network card adapter
+    MAC_addr = "00c0cab400dd"
+
+    long_interface_name = f"wlx{MAC_addr}"
+    short_interface_name = 'wlan1mon'
+
+    print(f"Long Interface Name: {long_interface_name}")
+    print(f"Short Interface Name: {short_interface_name}")
+    
+    #Check if monitor interface exists for long interface
+    process_long = subprocess.Popen(['iwconfig', long_interface_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output_long, _ = process_long.communicate()
+
+    # Run iwconfig for short interface
+    process_short = subprocess.Popen(['iwconfig', short_interface_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output_short, _ = process_short.communicate()
+
+    # Check if 'No such device' substring is present in both outputs
+    if b'No such device' in output_long and b'No such device' in output_short:
+        print(f"Didn't detect interface {long_interface_name} or {short_interface_name}")
+        exit(1)
+
+    # Default Wi-Fi channel to scan
+    default_wifi_channel = 6
+
+    # TODO: set up monitor mode
+        # TODO: if dongle not connected, exit
+        # Command to run
+    command_long = f"sudo airmon-ng start {long_interface_name} 1 && airmon-ng check kill"
+    command_short = f"sudo airmon-ng start {short_interface_name} 1 && airmon-ng check kill"
+
+    #Run the command to start monitor mode
+    try:
+        subprocess.run(command_short, shell=True, check=True)
+        subprocess.run(command_long, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        
+    # Get list of Wi-Fi channels
+    wifi_channel_list = get_channel_list()
+
+    #active_channel_list = []
+
+    initial_detection = True
+    
+    # This section loops through the WiFi Channels.It scans channels 1, 6, and 11 for 0.5
+    # seconds each, scans 4  of the 5Ghz for 0.25 seconds each, returns to the 2.4 GHz channels
+    # again for 0.5 seconds each, and then scans the last 4 5GHz channels for 0.25 seconds each.
+    # This 5 second scanning patern then repeats'''
+    
+    while initial_detection:
+        
+        for channel in wifi_channel_list:
+            # TODO: switch to channel
+            # TODO: add sudo capability
+            # TODO: add dict so not doing string conversion
+            print(["airmon-ng", "start", "wlan1mon", channel])
+            print(f'Scanning Channel: {channel}')
+
+            if channel == '1' or channel == '6' or channel == '11':
+                time.sleep(5)
+                #print('sleep for 5 s')
+            else:
+                time.sleep(2)
+                #print('sleep for 2 s')
+
+
+    # TODO: end packet capture
+
+
+if __name__ == "__main__":
+    main()
