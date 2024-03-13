5d1f0c7a5d71a076c45fb77b51c99527122c6fe8
MitchellBuck <94646521+MitchellBuck@users.noreply.github.com>
2024-03-13 13:30:36 -0600

    Add files via upload

--- src/cursed-remoteid/pcap_reader_pyshark.py
+++ src/cursed-remoteid/pcap_reader_pyshark.py
@@ -7,6 +7,7 @@ import boto3 #need to install sperately
 import math #included in base python
 from botocore.exceptions import ClientError
 import time #included in python
+import calendar #included
 
 
   
@@ -19,7 +20,7 @@ while x<2:
         csvPath=path[:-6]+'csv'
         with open(csvPath, 'w', newline='',encoding="utf-8") as file:
             writer = csv.writer(file)
-            field = ["Access Address","TimeStamp","ID","direction", "speed", "vspeed","lat","long"]
+            field = ["Access Address","ID","TimeStamp","direction", "speed", "vspeed","lat","long"]
             writer.writerow(field)
             for pkt in pcap:
                 try:
@@ -27,10 +28,14 @@ while x<2:
                 except KeyError:
                     access_address=pkt['WLAN'].get_field('wlan.sa_resolved')
                 try:
-                    B = pkt.frame_info.time_utc[:15]
-                    C=math.floor(int(pkt['OpenDroneID'].get_field('OpenDroneID.loc_timeStamp'))/10/60)
-                    D=int(pkt['OpenDroneID'].get_field('OpenDroneID.loc_timeStamp'))/10-(C*60)
-                    TimeStamp= B+':'+str(C)+':'+str(D)
+                    temp = pcap[3].frame_info.time_utc[:3]
+                    Month=str(list(calendar.month_abbr).index(temp))
+                    Day=str(pcap[3].frame_info.time_utc[4:6])
+                    Year=str(pcap[3].frame_info.time_utc[8:12])
+                    B = pkt.frame_info.time_utc[13:15]
+                    Minute=math.floor(int(pkt['OpenDroneID'].get_field('OpenDroneID.loc_timeStamp'))/10/60)
+                    Second=math.floor(int(pkt['OpenDroneID'].get_field('OpenDroneID.loc_timeStamp'))/10-(Minute*60))
+                    TimeStamp= Year+'-'+Month+'-'+Day+' '+B+':'+str(Minute)+':'+str(Second)
                     ID=pkt['OpenDroneID'].get_field('OpenDroneID.basicID_id_asc')
                     direction=pkt['OpenDroneID'].get_field('OpenDroneID.loc_direction')
                     speed=pkt['OpenDroneID'].get_field('OpenDroneID.loc_speed')
@@ -39,15 +44,19 @@ while x<2:
                     lon=pkt['OpenDroneID'].get_field('OpenDroneID.loc_lon')
                 except TypeError:
                     pass
-                writer.writerow([access_address,TimeStamp,ID,direction,speed,vspeed,lat,lon])
+                if (Minute < 60):
+                    if str(ID).isascii():
+                        writer.writerow([access_address,ID,TimeStamp,direction,speed,vspeed,lat,lon])
+                    else:
+                        continue
         pcap.close()
-        os.remove(path)
+        #os.remove(path)
         s3 = boto3.client("s3",
-                aws_access_key_id='GET FROM AWS',
-                aws_secret_access_key='GET FROM AWS')
+                aws_access_key_id='',
+                aws_secret_access_key='')
         start_time = time.time_ns()
         s3.upload_file(csvPath, "cursed-remoteid-data",csvPath[-28:])
         end_time = time.time_ns()
         print(f'Elapsed upload time: {end_time-start_time:d} ns')
-        os.remove(csvPath)
+        #os.remove(csvPath)
     x=x+1
